29cb52dfcab76267d6f7b4805fe6d0a84ba9e020
EntenteCordial <ayaayaaya686@gmail.com>
2017-03-17 20:43:26 +0700

    Add lines + emotions

--- amadeus.lua
+++ amadeus.lua
@@ -37,20 +37,30 @@ state = new_state
 file:close()
 
 if state == 'hello' then
+eyes = 'happy'
+mouth = 'happy'
 text = "Hello there! I'm Makise Kurisu, nice to meet you!"
-updated = true
---elseif state == 'goodbye' then
+elseif state == 'nullpo' or state == 'nurupo' then
+eyes = ''
+mouth = 'suprised'
+text = 'Gah!'
+elseif string.find(state, 'christina') then
+eyes = 'serious'
+mouth = 'neutral'
+specific_line = { 'Stop adding -tina!', "Don't call me that!" }
+text = specific_line[math.random(#specific_line)]
+elseif string.find(state, 'atchan') or string.find(state, 'atchannel') or string.find(state, 'kurigohan') or string.find(state, 'kamehameha') then
+text = "I'm still not happy about that."
 elseif state == 'goodbye' then
+eyes = 'happy'
+mouth = 'happy'
 text='Goodbye!'
-updated = true
 end
 
-if updated then
 text = wrap(text, width)
 buffer = pre
 tick = 0
 updated = false
-end
 --tick = 0
 
 end
