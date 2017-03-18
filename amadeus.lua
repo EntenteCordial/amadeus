b7010a432499e82b570c5fd75842cde48e1731a5
EntenteCordial <ayaayaaya686@gmail.com>
2017-03-18 12:25:43 +0700

    Add tabbing

--- amadeus.lua
+++ amadeus.lua
@@ -8,6 +8,7 @@ local state = ''
 local text = "Hello there! I'm Makise Kurisu, nice to meet you!"
 local buffer = pre
 local tick = 0
+local length = 0
 local updated = false
 
 local eyes = 'happy'
@@ -15,70 +16,82 @@ local mouth = 'happy'
 
 --http://lua-users.org/wiki/StringRecipes
 function wrap(str, limit)
-local here = 1
-  return str:gsub("(%s+)()(%S+)()",
-                          function(sp, st, word, fi)
-                            if fi-here > limit then
-                              here = st
-                              return "\n"..word
-                            end
-                          end)
+	local here = 1
+	return str:gsub("(%s+)()(%S+)()",
+	function(sp, st, word, fi)
+		if fi-here > limit then
+			here = st
+			return "\n"..word
+		end
+	end)
 end
 
 function update_text()
 
-local file = io.open('input.txt', 'rb')
-local new_state = file:read('*l')
-if new_state == state then 
-file:close()
-return
-end
-state = new_state
-file:close()
+	local file = io.open('input.txt', 'rb')
+	local new_state = file:read('*l')
+	if new_state == state then 
+		file:close()
+		return
+	end
+	state = new_state
+	file:close()
 
-if state == 'hello' then
-eyes = 'happy'
-mouth = 'happy'
-text = "Hello there! I'm Makise Kurisu, nice to meet you!"
-elseif state == 'nullpo' or state == 'nurupo' then
-eyes = ''
-mouth = 'suprised'
-text = 'Gah!'
-elseif string.find(state, 'christina') then
-eyes = 'serious'
-mouth = 'neutral'
-specific_line = { 'Stop adding -tina!', "Don't call me that!" }
-text = specific_line[math.random(#specific_line)]
-elseif string.find(state, 'atchan') or string.find(state, 'atchannel') or string.find(state, 'kurigohan') or string.find(state, 'kamehameha') then
-text = "I'm still not happy about that."
-elseif state == 'goodbye' then
-eyes = 'happy'
-mouth = 'happy'
-text='Goodbye!'
-end
+	if state == '' then
+	return
+	end
 
-text = wrap(text, width)
-buffer = pre
-tick = 0
-updated = false
---tick = 0
+	if state == 'hello' then
+		eyes = 'happy'
+		mouth = 'happy'
+		text = "Hello there! I'm Makise Kurisu, nice to meet you!"
+	elseif state == 'nullpo' or state == 'nurupo' then
+		eyes = ''
+		mouth = 'suprised'
+		text = 'Gah!'
+	elseif string.find(state, 'christina') then
+		eyes = 'serious'
+		mouth = 'neutral'
+		specific_line = { 'Stop adding -tina!', "Don't call me that!" }
+		text = specific_line[math.random(#specific_line)]
+	elseif string.find(state, 'atchan') or string.find(state, 'atchannel') or string.find(state, 'kurigohan') or string.find(state, 'kamehameha') then
+		text = "I'm still not happy about that."
+	elseif state == 'goodbye' then
+		eyes = 'happy'
+		mouth = 'happy'
+		text='Goodbye!'
+	end
+
+	text = wrap(text, width)
+	buffer = pre
+	tick = 0
+	updated = false
+	--tick = 0
 
 end
 
 function conky_string_func()
-update_text()
-tick = tick + 1
-if text:sub(tick, tick) == '\n' then
-buffer = buffer .. '\n' .. pre
-else
-buffer = buffer .. text:sub(tick, tick)
-end
-return buffer
+	if math.mod(tick, 10) == 0 then 
+		update_text()
+	end
+
+	if state == '' then
+		tick = tick + 1
+		buffer = pre .. string.gsub(text, '\n', '\n' .. pre)
+	else
+		tick = tick + 1
+		if text:sub(tick, tick) == '\n' then
+			buffer = buffer .. '\n' .. pre
+		else
+			buffer = buffer .. text:sub(tick, tick)
+		end
+	end
+	return buffer
 end
 
 function conky_image_func()
-return '${image ./eyes/' .. eyes .. '.png -p 930,192 -s 569x128}' ..
-'${image ./mouth/' .. mouth .. '.png -p 933,425 -s 569x65}'
+	return '${image ./eyes/' .. eyes .. '.png -p 930,192 -s 569x128}' ..
+	'${image ./mouth/' .. mouth .. '.png -p 933,425 -s 569x65}'
 end
 
 end
